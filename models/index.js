52685412fcc8f4bf47cafbdb2ff4ced3f37fbddc
Nathan_Wichmann <nathanwicker@hotmail.com>
2021-05-17 21:20:11 -0400

    updates

--- models/index.js
+++ models/index.js
@@ -4,6 +4,37 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+
+// The association can also be created from the Car side
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  
+});
+
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'add_tag'
+});
+
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  // Define an alias for when data is retrieved
+  as: 'added_tag'
+});
+
 // Products belongsTo Category
 
 // Categories have many Products
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,61 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    include: [Product],
+  })
+  .then((categories) => {
+      res.json(categories)
+    }
   // be sure to include its associated Products
-});
+  );
+
+})
+// api/catergories/6
 
 router.get('/:id', (req, res) => {
+  console.log(req.params)
+
+  Category.findOne({
+    where: {
+      id:req.params.id
+    },
+    include: [Product]
+  })
+
+  .then( (category) => {
+    res.json(category)
+  })
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
 router.post('/', (req, res) => {
+  Category.create(req.body)
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err))
   // create a new category
 });
 
 router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((category) => res.status(200).json(category))
+  .catch((err) => res.status(400).json(err))
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
   // delete a category by its `id` value
+  
 });
 
 module.exports = router;
